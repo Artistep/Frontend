49471af5389ac847f18f98a999d29a279a52d625
denebe <twhjdfo01@daum.net>
2022-09-15 18:49:41 +0900

    [fix]Right sidebar

--- src/components/rightsidebar/index.jsx
+++ src/components/rightsidebar/index.jsx
@@ -1,9 +1,36 @@
-import { SideBox, SideContainer } from "../Box/styled";
+import {
+  SideBox,
+  SideContainer,
+  HeaderBox,
+  Circle,
+  HeaderBtext,
+  HeaderStext,
+} from "../Box/styled";
+import { BigText } from "../Label/styled";
 
 const RightSideBar = () => {
   return (
     <SideContainer id="right">
       <SideBox id="title">내 계정</SideBox>
+      <HeaderBox>
+        <Circle></Circle>
+        <div>
+          <HeaderBtext>활동명</HeaderBtext>
+          <HeaderStext>@WORKING_NAME</HeaderStext>
+        </div>
+      </HeaderBox>
+      <div style={{marginTop: "20px"}}>
+        <div style={{marginBottom: "10px"}}>회원님을 위한 컨택 추천</div>
+        <div>
+          <HeaderBox>
+            <Circle></Circle>
+            <div>
+              <HeaderBtext>활동명</HeaderBtext>
+              <HeaderStext>@WORKING_NAME</HeaderStext>
+            </div>
+          </HeaderBox>
+        </div>
+      </div>
     </SideContainer>
   );
 };
