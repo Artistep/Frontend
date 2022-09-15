82da03d4a9ca805c242d54700dbefb4d25ad878b
denebe <twhjdfo01@daum.net>
2022-09-15 16:11:41 +0900

    [fix]navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,6 +1,11 @@
+import React, { useState } from "react";
 import Logo from "../../assets/img/ArtistepLogo.png";
 
 const Nav = () => {
+  const [state, setState] = useState(true);
+  const onClick = () => {
+    setState(!state);
+  };
   return (
     <div
       style={{
@@ -11,9 +16,27 @@ const Nav = () => {
         padding: "10px",
       }}
     >
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <img src={Logo} alt="Logo" />
-      </div>
+      {state ? (
+        <>
+          <div style={{ display: "flex", justifyContent: "space-between", width: "60%" ,  margin: "0 auto"}}>
+            <button>+</button>
+            <img src={Logo} alt="Logo" onClick={onClick} />
+            <button>+</button>
+          </div>
+        </>
+      ) : (
+        <>
+        <div style={{height: "60px", display: "flex", justifyContent: "center"}}>
+        <h1 style={{alignSelf: "center"}}>  로그인</h1>
+
+        </div>
+
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            
+            <img src={Logo} alt="Logo" onClick={onClick} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
